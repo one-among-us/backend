892ab6b779f4bf74530d766f5fb4555ff4fb66b5
Hykilpikonna <me@hydev.org>
2021-11-12 12:06:11 -0500

    [+] Implement flowers

--- src/main/kotlin/org/hydev/back/controller/FlowerController.kt
+++ src/main/kotlin/org/hydev/back/controller/FlowerController.kt
@@ -1,6 +1,8 @@
 package org.hydev.back.controller
 
 import org.hydev.back.H
+import org.hydev.back.db.Flower
+import org.hydev.back.db.FlowerRepo
 import org.hydev.back.str
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,11 +10,31 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/flowers")
-class FlowerController
+class FlowerController(
+    private val flowerRepo: FlowerRepo
+)
 {
     @GetMapping("/get")
-    fun get(@H username: str): Any
+    fun get(@H id: str): Any
     {
-        return "Not implemented"
+        val flower = flowerRepo.queryByPersonId(id) ?: return "None"
+        return flower.flowers
+    }
+
+    @GetMapping("/give")
+    fun give(@H id: str): Any
+    {
+        var flower = flowerRepo.queryByPersonId(id)
+        if (flower == null)
+        {
+            flower = Flower(personId = id, flowers = 1)
+            flowerRepo.save(flower)
+        }
+        else
+        {
+            flower.flowers += 1
+            flowerRepo.save(flower)
+        }
+        return "{}"
     }
 }
--- src/main/kotlin/org/hydev/back/db/Flower.kt
+++ src/main/kotlin/org/hydev/back/db/Flower.kt
@@ -3,6 +3,7 @@ package org.hydev.back.db
 import org.hydev.back.int
 import org.hydev.back.str
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
@@ -10,9 +11,13 @@ import javax.persistence.Id
 @Entity
 data class Flower(
     @Id @GeneratedValue
-    var pk: int = 0,
-    var id: str = "",
+    var id: int = 0,
+    var personId: str = "",
     var flowers: int = 0
 )
 
+@Repository
 interface FlowerRepo: JpaRepository<Flower, int>
+{
+    fun queryByPersonId(personId: str): Flower?
+}

1ee2b3c1716c3fc53c60619a73e9de6e438094ba
Hykilpikonna <me@hydev.org>
2021-11-12 09:49:27 -0500

    Initial commit

---
+++ src/main/kotlin/org/hydev/plugins/Routing.kt
@@ -0,0 +1,17 @@
+package org.hydev.plugins
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun Application.configureRouting()
+{
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}

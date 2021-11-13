e5d8f5ac72fc4f987f0196281de448b0a87517af
Hykilpikonna <me@hydev.org>
2021-11-12 22:13:30 -0500

    [+] Date format util method

--- src/main/kotlin/org/hydev/back/Extensions.kt
+++ src/main/kotlin/org/hydev/back/Extensions.kt
@@ -2,6 +2,8 @@ package org.hydev.back
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestParam
+import java.text.SimpleDateFormat
+import java.util.*
 
 typealias H = RequestParam
 
@@ -12,3 +14,4 @@ typealias list<T> = ArrayList<T>
 
 
 fun <T> T.http(code: Int): ResponseEntity<T> = ResponseEntity.status(code).body<T>(this)
+fun date(f: str = "yyyy-MM-dd"): str = SimpleDateFormat(f).format(Date())

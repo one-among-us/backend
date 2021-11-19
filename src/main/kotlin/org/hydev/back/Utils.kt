207756f0d605b4e1f31479392f2ec0482dbd8c56
Hykilpikonna <me@hydev.org>
2021-11-19 14:28:28 -0500

    [-] Remove unused variables

--- src/main/kotlin/org/hydev/back/Utils.kt
+++ src/main/kotlin/org/hydev/back/Utils.kt
@@ -2,19 +2,12 @@ package org.hydev.back
 
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.jackson.responseObject
-import org.springframework.web.util.UriComponentsBuilder
 
 data class CaptchaResponse(val success: bool)
 
 fun verifyCaptcha(secret: str, response: str?): bool
 {
     if (response == null) return false
-
-    val url = UriComponentsBuilder
-        .fromUriString("https://www.recaptcha.net/recaptcha/api/siteverify")
-        .queryParam("secret", secret)
-        .queryParam("response", response)
-        .build().toUri()
 
     val (req, res, result) = "https://www.recaptcha.net/recaptcha/api/siteverify"
         .httpGet(listOf("secret" to secret, "response" to response))

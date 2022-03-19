78efb9565c735f60246cba3b1a85e5e61e8ceccb
Hykilpikonna <me@hydev.org>
2022-03-19 17:04:33 -0400

    [+] Add kotlin telegram bot dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
+	maven { setUrl("https://jitpack.io") }
 }
 
 dependencies {
@@ -24,21 +25,26 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.6")
 
+	// Database
 	// https://my.oschina.net/mingyuelab/blog/3190313
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.5.6")
 	implementation("org.xerial:sqlite-jdbc:3.36.0.2")
 	implementation("com.github.gwenn:sqlite-dialect:0.1.2")
+	// https://mvnrepository.com/artifact/org.mariadb.jdbc/mariadb-java-client
+	implementation("org.mariadb.jdbc:mariadb-java-client:2.7.4")
 
+	// Github Client
 	// https://mvnrepository.com/artifact/org.kohsuke/github-api
 	implementation("org.kohsuke:github-api:1.135")
 	// https://mvnrepository.com/artifact/org.eclipse.jgit/org.eclipse.jgit
 	implementation("org.eclipse.jgit:org.eclipse.jgit:5.13.0.202109080827-r")
-	// https://mvnrepository.com/artifact/org.mariadb.jdbc/mariadb-java-client
-	implementation("org.mariadb.jdbc:mariadb-java-client:2.7.4")
 
 	// https://mvnrepository.com/artifact/com.github.kittinunf.fuel/fuel
 	implementation("com.github.kittinunf.fuel:fuel:2.3.1")
 	implementation("com.github.kittinunf.fuel:fuel-jackson:2.3.1")
+
+	// Kotlin Telegram Bot
+	implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.6")
 }
 
 tasks.withType<KotlinCompile> {

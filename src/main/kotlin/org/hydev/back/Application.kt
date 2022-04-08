dbc988a673f4e182cb3ad940c112c6fb1ba21069
Hykilpikonna <me@hydev.org>
2022-04-08 15:44:19 -0400

    [O] Separate callback

--- src/main/kotlin/org/hydev/back/Application.kt
+++ src/main/kotlin/org/hydev/back/Application.kt
@@ -5,14 +5,9 @@ import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.callbackQuery
 import com.github.kotlintelegrambot.dispatcher.command
-import com.github.kotlintelegrambot.dispatcher.handlers.HandleCallbackQuery
 import com.github.kotlintelegrambot.entities.ChatId
-import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
-import com.github.kotlintelegrambot.entities.ParseMode
-import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
 import com.github.kotlintelegrambot.logging.LogLevel
-import com.github.kotlintelegrambot.network.fold
-import org.hydev.back.db.PendingCommentRepo
+import org.hydev.back.controller.CommentController
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Component
@@ -25,59 +20,19 @@ lateinit var bot: Bot
 class Application
 
 @Component
-class PostConstruct(private val commentRepo: PendingCommentRepo)
+class PostConstruct(private val commentController: CommentController)
 {
 	@PostConstruct
 	fun init()
 	{
-		// Callback when the administrator clicks pass or reject
-		val commentCallback: HandleCallbackQuery = {
-			val pass = callbackQuery.data == "comment-pass"
-			val chatId = ChatId.fromId(callbackQuery.message!!.chat.id)
-			val msgId = callbackQuery.message!!.messageId
-			val inlId = callbackQuery.inlineMessageId
-			val message = callbackQuery.message!!.text!!
-			val id = message.split(" ")[0].substring(1).toLong()
-
-			bot.editMessageReplyMarkup(chatId, msgId, inlId, null)
-
-			if (pass)
-			{
-				// Commit changes
-				var statusMsgId = 0L
-				bot.sendMessage(chatId, "正在提交更改...").fold({ statusMsgId = it!!.result!!.messageId })
-				val comment = commentRepo.queryById(id)!!
-				val fName = date("yyyy-MM-dd") + "-${comment.id}.txt"
-				val fPath = "people/${comment.personId}/comments/${fName}"
-				val cMsg = "[+] Comment added by ${comment.submitter} for ${comment.personId}"
-				val url = commitDirectly(comment.submitter, DataEdit(fPath, comment.content), cMsg)
-				bot.deleteMessage(chatId, statusMsgId)
-
-				// Update database
-				comment.approved = true
-				commentRepo.save(comment)
-
-				// Attach URL
-				bot.editMessageText(chatId, msgId, inlId, "$message\n- 已通过审核✅", replyMarkup =
-					InlineKeyboardMarkup.createSingleRowKeyboard(
-						InlineKeyboardButton.Url(
-							text = "查看 Commit",
-							url = url
-						)
-					)
-				)
-			}
-			else bot.editMessageText(chatId, msgId, inlId, "$message\n- 已删除❌")
-		}
-
 		// Create bot
 		bot = bot {
 			token = secrets.telegramBotToken
 			dispatch {
 				logLevel = LogLevel.Error
 				command("start") { bot.sendMessage(ChatId.fromId(message.chat.id), "🐱") }
-				callbackQuery("comment-pass", commentCallback)
-				callbackQuery("comment-reject", commentCallback)
+				callbackQuery("comment-pass", commentController.commentCallback)
+				callbackQuery("comment-reject", commentController.commentCallback)
 			}
 		}
 		bot.sendMessage(ChatId.fromId(secrets.telegramChatID), "Server Started.")
--- src/main/kotlin/org/hydev/back/controller/CommentController.kt
+++ src/main/kotlin/org/hydev/back/controller/CommentController.kt
@@ -2,11 +2,12 @@
 
 package org.hydev.back.controller
 
+import com.github.kotlintelegrambot.dispatcher.handlers.HandleCallbackQuery
 import com.github.kotlintelegrambot.entities.ChatId
 import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
 import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
+import com.github.kotlintelegrambot.network.fold
 import org.hydev.back.*
-import org.hydev.back.db.FlowerRepo
 import org.hydev.back.db.PendingComment
 import org.hydev.back.db.PendingCommentRepo
 import org.springframework.web.bind.annotation.CrossOrigin
@@ -30,6 +31,48 @@ class CommentController(private val commentRepo: PendingCommentRepo)
             callbackData = "comment-reject"
         )
     )
+
+    val commentCallback: HandleCallbackQuery = callback@ {
+        val pass = callbackQuery.data == "comment-pass"
+        val chatId = ChatId.fromId(callbackQuery.message!!.chat.id)
+        val msgId = callbackQuery.message!!.messageId
+        val inlId = callbackQuery.inlineMessageId
+        val message = callbackQuery.message!!.text!!
+        val id = message.split(" ")[0].substring(1).toLong()
+
+        bot.editMessageReplyMarkup(chatId, msgId, inlId, null)
+
+        // Not pass, remove
+        if (!pass)
+        {
+            bot.editMessageText(chatId, msgId, inlId, "$message\n- 已删除❌")
+            return@callback
+        }
+
+        // Commit changes
+        var statusMsgId = 0L
+        bot.sendMessage(chatId, "正在提交更改...").fold({ statusMsgId = it!!.result!!.messageId })
+        val comment = commentRepo.queryById(id)!!
+        val fPath = "people/${comment.personId}/comments/${date("yyyy-MM-dd")}-C${comment.id}.json"
+        val cMsg = "[+] Comment added by ${comment.submitter} for ${comment.personId}"
+        val content =
+        val url = commitDirectly(comment.submitter, DataEdit(fPath, comment.content), cMsg)
+        bot.deleteMessage(chatId, statusMsgId)
+
+        // Update database
+        comment.approved = true
+        commentRepo.save(comment)
+
+        // Attach URL
+        bot.editMessageText(chatId, msgId, inlId, "$message\n- 已通过审核✅", replyMarkup =
+            InlineKeyboardMarkup.createSingleRowKeyboard(
+                InlineKeyboardButton.Url(
+                    text = "查看 Commit",
+                    url = url
+                )
+            )
+        )
+    }
 
     @PostMapping("/add")
     fun addComment(

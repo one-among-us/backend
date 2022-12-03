c7ec63579d86ce4e4efc36a9a9bdab98562a0b05
Hykilpikonna <me@hydev.org>
2022-12-03 00:17:16 -0500

    [F] Rework edit controller

--- src/main/kotlin/org/hydev/back/controller/EditController.kt
+++ src/main/kotlin/org/hydev/back/controller/EditController.kt
@@ -2,6 +2,9 @@
 
 package org.hydev.back.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.github.kotlintelegrambot.entities.ChatId
 import org.hydev.back.*
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,19 +21,50 @@ class EditController
     fun get(@P id: str, @P content: str, @P captcha: str, @P name: str, @P email: str,
             request: HttpServletRequest): Any
     {
+        val ip = request.getIP()
+        println("""
+[+] Info edit received. 
+> IP: $ip
+> ID: $id
+> Name: $name
+> Email: $email
+> Content: $content
+<< EOF >>""")
+
         // Verify captcha
         if (!verifyCaptcha(secrets.recaptchaSecret, captcha))
+        {
+            println("> Rejected: Cannot verify captcha")
             return "没有查到验证码".http(400)
+        }
 
         // TODO: Check if id exists
-        val name = name.ifBlank { "Anonymous" } + " ${request.remoteAddr}"
+        val name = name.ifBlank { "Anonymous" }
         val email = if (email.isBlank() || !email.isValidEmail())
             "anonymous@example.com" else email
 
+        // Convert json to yml
+        val obj = ObjectMapper().readTree(content)
+        val yml = ObjectMapper(YAMLFactory()).writeValueAsString(obj)
+
+        val notif = """
+$id 收到了信息编辑请求：
+
+$yml
+
+- IP: $ip
+- 姓名: $name
+- 邮箱: $email"""
+
         return try
         {
-            createPullRequest(name, email,
-                arrayListOf(DataEdit("people/$id/info.json5", content)))
+            bot.sendMessage(ChatId.fromId(secrets.telegramChatID), notif)
+
+            // This fails for some reason:
+            // createPullRequest(name, email,
+            //     arrayListOf(DataEdit("people/$id/info.json5", content)))
+
+            "Success".http(200)
         }
         catch (e: Exception) { "创建更改请求失败（${e.message}）".http(500) }
     }

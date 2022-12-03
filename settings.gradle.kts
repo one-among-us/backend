af93beb1cf7fae8ce82abca97287579cc13bf5d8
Hykilpikonna <me@hydev.org>
2022-12-03 00:17:42 -0500

    [U] Upgrade gradle dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,5 @@ rootProject.name = "OneAmongUs.Backend"
 
 plugins {
     // Automatic updater
-    id("de.fayard.refreshVersions") version "0.40.1"
+    id("de.fayard.refreshVersions") version "0.51.0"
 }

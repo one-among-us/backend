6fc41943b23bd31e53edf6c97e7a44f77fafc694
Azalea Gui <me@hydev.org>
2023-01-13 18:37:39 -0500

    [+] Friendly text for harmlevel

--- src/main/kotlin/org/hydev/back/ai/HarmLevel.kt
+++ src/main/kotlin/org/hydev/back/ai/HarmLevel.kt
@@ -1,5 +1,10 @@
 package org.hydev.back.ai
 
-enum class HarmLevel {
-    SAFE, HARMFUL, MAYBE, OFFLINE, INVALID, TOO_LONG
+enum class HarmLevel(msg: String? = null) {
+    SAFE,
+    HARMFUL("❌ AI 标记: 有害"),
+    MAYBE("⚠️ AI 标记: 可能有害"),
+    OFFLINE("🔌 AI 连接失败"),
+    INVALID("😕 AI 没有理解"),
+    TOO_LONG("😴 AI 因为太长无法分析")
 }
